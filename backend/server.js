2320a1ab3d94fa6c56fb73755a8ebac2e7403738
Vanjula <vanjulasankar@gmail.com>
2025-03-06 16:13:19 +0530

    server code utpo home page

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
 const bookRoutes = require("./routes/bookRoutes");
+const bookDetailsRoutes =require("./routes/bookDetailsRoutes");
 
 dotenv.config();
 const app = express();
@@ -18,6 +19,8 @@ connectDB();
 // Routes
 app.use("/api/auth", authRoutes);
 app.use("/api", bookRoutes);
+app.use("/api/book",bookDetailsRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+    
